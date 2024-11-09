344bda67b59110caebe81360b210edc1bbbaab1f
jcondotta <jefferson.condotta@gmail.com>
2024-11-09 21:01:16 +0100

    Initial tests for graalvm

--- src/main/java/com/jcondotta/BankAccountApplication.java
+++ src/main/java/com/jcondotta/BankAccountApplication.java
@@ -1,10 +1,14 @@
 package com.jcondotta;
 
+import io.micronaut.context.env.Environment;
 import io.micronaut.runtime.Micronaut;
 
 public class BankAccountApplication {
 
 	public static void main(String[] args) {
-		Micronaut.run(BankAccountApplication.class, args);
+		Micronaut.build(args)
+				.mainClass(BankAccountApplication.class)
+				.defaultEnvironments(Environment.DEVELOPMENT)
+				.start();
 	}
 }

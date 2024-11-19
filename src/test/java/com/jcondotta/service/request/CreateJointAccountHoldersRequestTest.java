e2cd58a8e54b500bf275273c5b2b3de93be97a3b
jcondotta <jefferson.condotta@gmail.com>
2024-11-19 13:24:43 +0100

    CreateJointAccountHolder Request Unit tests

--- src/test/java/com/jcondotta/service/request/CreateJointAccountHoldersRequestTest.java
+++ src/test/java/com/jcondotta/service/request/CreateJointAccountHoldersRequestTest.java
@@ -16,11 +16,11 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class CreateJointAccountHoldersRequestTest {
 
     private static final UUID BANK_ACCOUNT_ID_BRAZIL = TestBankAccountId.BRAZIL.getBankAccountId();
-
     private static final String ACCOUNT_HOLDER_NAME_JEFFERSON = TestAccountHolderRequest.JEFFERSON.getAccountHolderName();
     private static final String PASSPORT_NUMBER_JEFFERSON = TestAccountHolderRequest.JEFFERSON.getPassportNumber();
     private static final LocalDate DATE_OF_BIRTH_JEFFERSON = TestAccountHolderRequest.JEFFERSON.getDateOfBirth();
@@ -45,10 +45,9 @@ public class CreateJointAccountHoldersRequestTest {
     void shouldNotDetectConstraintViolation_whenAccountHoldersListHasMultipleItems() {
         var jeffersonAccountHolderRequest = TestAccountHolderRequest.JEFFERSON.toAccountHolderRequest();
         var virginioAccountHolderRequest = TestAccountHolderRequest.VIRGINIO.toAccountHolderRequest();
+        var accountHolderRequests = List.of(jeffersonAccountHolderRequest, virginioAccountHolderRequest);
 
-        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(
-                BANK_ACCOUNT_ID_BRAZIL, List.of(jeffersonAccountHolderRequest, virginioAccountHolderRequest)
-        );
+        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(BANK_ACCOUNT_ID_BRAZIL, accountHolderRequests);
 
         var constraintViolations = VALIDATOR.validate(createJointAccountHolderRequest);
         assertThat(constraintViolations).isEmpty();
@@ -64,107 +63,89 @@ public class CreateJointAccountHoldersRequestTest {
 
     @Test
     void shouldDetectConstraintViolation_whenAccountHoldersListHasMoreThan2Items() {
-        var jeffersonAccountHolderRequest = TestAccountHolderRequest.JEFFERSON.toAccountHolderRequest();
-        var virginioAccountHolderRequest = TestAccountHolderRequest.VIRGINIO.toAccountHolderRequest();
-        var patrizioAccountHolderRequest = TestAccountHolderRequest.PATRIZIO.toAccountHolderRequest();
-        var accountHolderRequests = List.of(jeffersonAccountHolderRequest, virginioAccountHolderRequest, patrizioAccountHolderRequest);
-
-        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(
-                BANK_ACCOUNT_ID_BRAZIL, accountHolderRequests
+        var accountHolderRequests = List.of(
+                TestAccountHolderRequest.JEFFERSON.toAccountHolderRequest(),
+                TestAccountHolderRequest.VIRGINIO.toAccountHolderRequest(),
+                TestAccountHolderRequest.PATRIZIO.toAccountHolderRequest()
         );
+        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(BANK_ACCOUNT_ID_BRAZIL, accountHolderRequests);
 
         var constraintViolations = VALIDATOR.validate(createJointAccountHolderRequest);
+
         assertThat(constraintViolations)
                 .hasSize(1)
                 .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolders.tooMany");
-                    assertThat(violation.getPropertyPath().toString()).isEqualTo("accountHolderRequests");
-                });
+                .satisfies(violation -> assertAll(
+                        () -> assertThat(violation.getMessage()).isEqualTo("accountHolders.tooMany"),
+                        () -> assertThat(violation.getPropertyPath().toString()).isEqualTo("accountHolderRequests")
+                ));
     }
 
     @Test
     void shouldDetectConstraintViolation_whenBankAccountIdIsNull() {
         var accountHolderRequest = TestAccountHolderRequest.JEFFERSON.toAccountHolderRequest();
-        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(null, List.of(accountHolderRequest));
+        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(null, accountHolderRequest);
 
         var constraintViolations = VALIDATOR.validate(createJointAccountHolderRequest);
 
         assertThat(constraintViolations)
                 .hasSize(1)
                 .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("bankAccount.bankAccountId.notNull");
-                    assertThat(violation.getPropertyPath()).hasToString("bankAccountId");
-                });
+                .satisfies(violation -> assertAll(
+                        () -> assertThat(violation.getMessage()).isEqualTo("bankAccount.bankAccountId.notNull"),
+                        () -> assertThat(violation.getPropertyPath()).hasToString("bankAccountId")
+                ));
     }
 
     @Test
     void shouldDetectConstraintViolation_whenAccountHoldersListIsEmpty() {
-        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(
-                BANK_ACCOUNT_ID_BRAZIL, List.of()
-        );
+        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(BANK_ACCOUNT_ID_BRAZIL, List.of());
 
         var constraintViolations = VALIDATOR.validate(createJointAccountHolderRequest);
 
         assertThat(constraintViolations)
                 .hasSize(1)
                 .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolders.notEmpty");
-                    assertThat(violation.getPropertyPath().toString()).isEqualTo("accountHolderRequests");
-                });
-    }
-
-    @Test
-    void shouldDetectConstraintViolation_whenAccountHoldersListIsNull() {
-        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(BANK_ACCOUNT_ID_BRAZIL, (List<AccountHolderRequest>) null);
-
-        var constraintViolations = VALIDATOR.validate(createJointAccountHolderRequest);
-
-        assertThat(constraintViolations)
-                .hasSize(1)
-                .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolders.notEmpty");
-                    assertThat(violation.getPropertyPath().toString()).isEqualTo("accountHolderRequests");
-                });
+                .satisfies(violation -> assertAll(
+                        () -> assertThat(violation.getMessage()).isEqualTo("accountHolders.notEmpty"),
+                        () -> assertThat(violation.getPropertyPath().toString()).isEqualTo("accountHolderRequests")
+                ));
     }
 
     @Test
     void shouldDetectConstraintViolation_whenAccountHolderRequestsListContainsNullItem() {
-        List<AccountHolderRequest> accountHolderRequests = Arrays.asList(TestAccountHolderRequest.JEFFERSON.toAccountHolderRequest(), null);
-        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(
-                BANK_ACCOUNT_ID_BRAZIL, accountHolderRequests
+        List<AccountHolderRequest> accountHolderRequests = Arrays.asList(
+                TestAccountHolderRequest.JEFFERSON.toAccountHolderRequest(),
+                null
         );
+        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(BANK_ACCOUNT_ID_BRAZIL, accountHolderRequests);
 
         var constraintViolations = VALIDATOR.validate(createJointAccountHolderRequest);
 
         assertThat(constraintViolations)
                 .hasSize(1)
                 .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolder.notNull");
-                    assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[1].<list element>");
-                });
+                .satisfies(violation -> assertAll(
+                        () -> assertThat(violation.getMessage()).isEqualTo("accountHolder.notNull"),
+                        () -> assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[1].<list element>")
+                ));
     }
 
     @ParameterizedTest
     @ArgumentsSource(BlankValuesArgumentProvider.class)
     void shouldDetectConstraintViolation_whenAccountHolderNameIsBlank(String blankAccountHolderName) {
         var accountHolderRequest = new AccountHolderRequest(blankAccountHolderName, DATE_OF_BIRTH_JEFFERSON, PASSPORT_NUMBER_JEFFERSON);
-        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(
-                BANK_ACCOUNT_ID_BRAZIL, accountHolderRequest
-        );
+        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(BANK_ACCOUNT_ID_BRAZIL, accountHolderRequest);
 
         var constraintViolations = VALIDATOR.validate(createJointAccountHolderRequest);
+
         assertThat(constraintViolations)
                 .hasSize(1)
                 .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolder.accountHolderName.notBlank");
-                    assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].accountHolderName");
-                });
+                .satisfies(violation -> assertAll(
+                        () -> assertThat(violation.getMessage()).isEqualTo("accountHolder.accountHolderName.notBlank"),
+                        () -> assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].accountHolderName")
+                ));
     }
 
     @Test
@@ -180,45 +161,27 @@ public class CreateJointAccountHoldersRequestTest {
         assertThat(constraintViolations)
                 .hasSize(1)
                 .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolder.accountHolderName.tooLong");
-                    assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].accountHolderName");
-                });
-    }
-
-    @Test
-    void shouldDetectConstraintViolation_whenDateOfBirthIsNull() {
-        var accountHolderRequest = new AccountHolderRequest(ACCOUNT_HOLDER_NAME_JEFFERSON, null, PASSPORT_NUMBER_JEFFERSON);
-        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(
-                BANK_ACCOUNT_ID_BRAZIL, accountHolderRequest
-        );
-
-        var constraintViolations = VALIDATOR.validate(createJointAccountHolderRequest);
-        assertThat(constraintViolations)
-                .hasSize(1)
-                .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolder.dateOfBirth.notNull");
-                    assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].dateOfBirth");
-                });
+                .satisfies(violation -> assertAll(
+                        () -> assertThat(violation.getMessage()).isEqualTo("accountHolder.accountHolderName.tooLong"),
+                        () -> assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].accountHolderName")
+                ));
     }
 
     @Test
     void shouldDetectConstraintViolation_whenDateOfBirthIsInFuture() {
         LocalDate futureDate = LocalDate.now().plusDays(1);
         var accountHolderRequest = new AccountHolderRequest(ACCOUNT_HOLDER_NAME_JEFFERSON, futureDate, PASSPORT_NUMBER_JEFFERSON);
-        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(
-                BANK_ACCOUNT_ID_BRAZIL, accountHolderRequest
-        );
+        var createJointAccountHolderRequest = new CreateJointAccountHoldersRequest(BANK_ACCOUNT_ID_BRAZIL, accountHolderRequest);
 
         var constraintViolations = VALIDATOR.validate(createJointAccountHolderRequest);
+
         assertThat(constraintViolations)
                 .hasSize(1)
                 .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolder.dateOfBirth.past");
-                    assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].dateOfBirth");
-                });
+                .satisfies(violation -> assertAll(
+                        () -> assertThat(violation.getMessage()).isEqualTo("accountHolder.dateOfBirth.past"),
+                        () -> assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].dateOfBirth")
+                ));
     }
 
     @Test
@@ -233,10 +196,10 @@ public class CreateJointAccountHoldersRequestTest {
         assertThat(constraintViolations)
                 .hasSize(1)
                 .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolder.dateOfBirth.past");
-                    assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].dateOfBirth");
-                });
+                .satisfies(violation -> assertAll(
+                        () -> assertThat(violation.getMessage()).isEqualTo("accountHolder.dateOfBirth.past"),
+                        () -> assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].dateOfBirth")
+                ));
     }
 
     @Test
@@ -250,10 +213,10 @@ public class CreateJointAccountHoldersRequestTest {
         assertThat(constraintViolations)
                 .hasSize(1)
                 .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolder.passportNumber.notNull");
-                    assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].passportNumber");
-                });
+                .satisfies(violation -> assertAll(
+                        () -> assertThat(violation.getMessage()).isEqualTo("accountHolder.passportNumber.notNull"),
+                        () -> assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].passportNumber")
+                ));
     }
 
     @ParameterizedTest
@@ -268,9 +231,9 @@ public class CreateJointAccountHoldersRequestTest {
         assertThat(constraintViolations)
                 .hasSize(1)
                 .first()
-                .satisfies(violation -> {
-                    assertThat(violation.getMessage()).isEqualTo("accountHolder.passportNumber.invalidLength");
-                    assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].passportNumber");
-                });
+                .satisfies(violation -> assertAll(
+                        () -> assertThat(violation.getMessage()).isEqualTo("accountHolder.passportNumber.invalidLength"),
+                        () -> assertThat(violation.getPropertyPath()).hasToString("accountHolderRequests[0].passportNumber")
+                ));
     }
 }
